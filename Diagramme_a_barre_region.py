48ae511aab3c4ac9437dd153563d7f75e9a3b920
Clement Delafosse <clement.delafosse@univ-rouen.fr>
2025-11-06 10:54:22 +0100

    Diagrame barre nombre region fini

--- Diagramme_a_barre_region.py
+++ Diagramme_a_barre_region.py
@@ -1,12 +1,23 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(r"C:\Users\delafcl4\Documents\experimentations_5G.csv", encoding="Windows-1252", sep=";", engine="python")
+df.head()
 
 
-fichier_csv = pd.read_csv(r"C:\Users\delafcl4\Documents\experimentations_5G.csv", encoding="Windows-1252", sep=";", engine="python")
-fichier_csv.head()
+
+def frequence_region():
+    region_counts = df["Région"].value_counts()  
+    
+    plt.figure(figsize=(10,6))
+    region_counts.plot(kind='bar', color='skyblue', edgecolor='black')
+    plt.title("Nombre d'expérimentations 5G par région")
+    plt.xlabel("Région")
+    plt.ylabel("Fréquence")
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+    plt.show()
+frequence_region () 
 
 
-def frequence():
-    for i in fichier_csv():
-        frequence = fichier_csv.iloc[2,1]
 
-frequence()
