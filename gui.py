e20cd0a3823af83c604e9f3f03558b060fd4ec8d
MATISSE POTIER (Etudiant) <potiema3@ur.univ-rouen.fr>
2025-11-12 10:00:55 +0100

    first

---
+++ gui.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk # poue inserer une image
+
+window = tk.Tk()
+window.title("SAE 15")
+window.geometry("683x384")
+
+
+
+window.mainloop()
